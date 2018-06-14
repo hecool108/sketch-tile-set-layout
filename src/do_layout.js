9618b7c995ac32babdd5c04c0f086490982fd5cf
TigerWang <hecool108@gmail.com>
2018-06-14 17:52:47 +0800

    Ascend Sorted Layers

--- src/do_layout.js
+++ src/do_layout.js
@@ -5,7 +5,26 @@ export default function(context) {
   var maxH = 0;
   var greatL = null;
   var layerParent;
+  var sortedLayers = [];
   if(selectedLayers.length != 0){
+    for (let index = 0; index < selectedLayers.count(); index++) {
+      const element = selectedLayers[index];
+      sortedLayers.push({
+        name:element.name(),
+        layer:element
+      })
+    }
+    sortedLayers.sort((a,b)=>{
+      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    })
     for(var i = 0; i < selectedLayers.count(); i++){
       var layer = selectedLayers[i];
       layerParent = layer.parentGroup();
@@ -21,8 +40,8 @@ export default function(context) {
     var cIndex = 0;
     var rIndex = 0;
     
-    for(var i = 0; i < selectedLayers.count(); i++){
-      var layer = selectedLayers[i];
+    for(var i = 0; i < sortedLayers.length; i++){
+      var layer = sortedLayers[i]["layer"];
       var f = layer.frame();
       f.setX(cIndex*maxW + (maxW - f.width())/2);
       f.setY(rIndex*maxH + (maxH - f.height()));
@@ -36,4 +55,5 @@ export default function(context) {
     layerParent.frame().setHeight((rIndex + 1) * maxH);
     context.document.showMessage("The Max size of assets is w:"+maxW+" h:"+maxH);
   }
+
 }
